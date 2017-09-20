df54d7180ed0846587f5a08d0d2dcd89c6d3d6f9
zzuwangfeng <zzuwangfeng@163.com>
2017-09-20 11:25:17 +0800

    add project

---
+++ DbConnector/Calculate.h
@@ -0,0 +1,13 @@
+//
+//  Calculate.h
+//  DbConnector
+//
+//  Created by jack wong on 2017/9/20.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculate : NSObject
+int plus(int a, int b) ;
+@end
---
+++ DbConnector/Calculate.m
@@ -0,0 +1,16 @@
+//
+//  Calculate.m
+//  DbConnector
+//
+//  Created by jack wong on 2017/9/20.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import "Calculate.h"
+
+@implementation Calculate
+
+int plus(int a, int b)  {
+    return a + b;
+}
+@end
