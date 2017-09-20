20d607f20cd1f6300352a897249280f79718fced
zzuwangfeng <zzuwangfeng@163.com>
2017-09-20 11:17:24 +0800

    Initial Commit

---
+++ DbConnector/DbConnector.h
@@ -0,0 +1,19 @@
+//
+//  DbConnector.h
+//  DbConnector
+//
+//  Created by jack wong on 2017/9/20.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DbConnector.
+FOUNDATION_EXPORT double DbConnectorVersionNumber;
+
+//! Project version string for DbConnector.
+FOUNDATION_EXPORT const unsigned char DbConnectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DbConnector/PublicHeader.h>
+
+
